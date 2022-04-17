d6681db7df3babb6e1efc02c49cf294971f57d22
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2022-04-17 15:42:55 -0400

    use-builder: Add tests, fix bugs, add docs

--- use_builder/src/lib.rs
+++ use_builder/src/lib.rs
@@ -1,5 +1,61 @@
-use std::collections::hash_map::Entry;
-use std::collections::{HashMap, HashSet};
+//! A simple crate to build source code use sections by combining multiple use section inputs
+//! ```rust
+//! use assert_unordered::assert_eq_unordered;
+//! use quote::quote;
+//! use use_builder::{UseBuilder, UseItems};
+//!
+//! // #1 - Build a two or more use trees and convert into `UseItems` (wrapped `Vec<ItemUse>`)
+//!
+//! let use1 = quote! {
+//!     use crate::Test;
+//!     use std::error::{Error as StdError};
+//!     use std::fmt::Debug;
+//! };
+//!
+//! let use2 = quote! {
+//!     use syn::ItemUse;
+//!     use std::fmt::Display;
+//!     use crate::*;
+//! };
+//!
+//! let items1: UseItems = syn::parse2(use1).unwrap();
+//! let items2: UseItems = syn::parse2(use2).unwrap();
+//!
+//! // #2 - Parse, process, and extract into sections
+//!
+//! let builder = UseBuilder::from_uses(vec![items1, items2]);
+//! let (std_use, ext_use, crate_use) = builder.into_items_sections().unwrap();
+//!
+//! // #3 - Validate our response matches expectation
+//!
+//! let std_expected = quote! {
+//!     use std::error::Error as StdError;
+//!     use std::fmt::{Debug, Display};
+//! };
+//! let std_expected = syn::parse2::<UseItems>(std_expected).unwrap().into_inner();
+//!
+//! let ext_expected = quote! {
+//!     use syn::ItemUse;
+//! };
+//! let ext_expected = syn::parse2::<UseItems>(ext_expected).unwrap().into_inner();
+//!
+//! let crate_expected = quote! {
+//!     use crate::*;
+//! };
+//! let crate_expected = syn::parse2::<UseItems>(crate_expected).unwrap().into_inner();
+//!
+//! assert_eq_unordered!(std_expected, std_use);
+//! assert_eq_unordered!(ext_expected, ext_use);
+//! assert_eq_unordered!(crate_expected, crate_use);
+//! ```
+
+#![warn(missing_docs)]
+
+use quote::__private::TokenStream;
+use quote::{ToTokens, TokenStreamExt};
+
+use indexmap::IndexMap;
+use std::collections::HashSet;
 use std::error::Error as StdError;
 use std::{cmp, fmt, hash};
 
@@ -8,8 +64,25 @@ const CRATE: [&str; 3] = ["self", "super", "crate"];
 
 // *** UseItems ***
 
+/// An opaque type primarily used for parsing to get an inner `Vec<syn::ItemUse>` (however,
+/// [from_items](UseItems::from_items) can also be used for an existing [Vec] of items if parsing is
+/// not required). This type is the sole input into [UseBuilder].
 pub struct UseItems {
     items: Vec<syn::ItemUse>,
+}
+
+impl UseItems {
+    /// Instead of using syn parsing, this can be used to wrap an existing [Vec] of use items
+    #[inline]
+    pub fn from_items(items: Vec<syn::ItemUse>) -> Self {
+        Self { items }
+    }
+
+    /// Consume this value and emit the inner [Vec] of [syn::ItemUse]
+    #[inline]
+    pub fn into_inner(self) -> Vec<syn::ItemUse> {
+        self.items
+    }
 }
 
 impl syn::parse::Parse for UseItems {
@@ -18,10 +91,16 @@ impl syn::parse::Parse for UseItems {
         let mut items = Vec::with_capacity(5);
 
         while !input.is_empty() {
-            items.push(input.parse()?)
+            items.push(input.parse()?);
         }
 
         Ok(Self { items })
+    }
+}
+
+impl ToTokens for UseItems {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        tokens.append_all(&self.items);
     }
 }
 
@@ -37,7 +116,7 @@ impl IntoIterator for UseItems {
 
 // *** Use Entry ***
 
-#[derive(Clone, cmp::Eq, hash::Hash, cmp::PartialEq)]
+#[derive(Clone, Debug, cmp::Eq, hash::Hash, cmp::PartialEq)]
 enum UseKey {
     Name(syn::Ident),
     Rename(syn::Ident, syn::Ident),
@@ -46,7 +125,7 @@ enum UseKey {
 
 // *** Use Data ***
 
-#[derive(Clone, cmp::Eq, hash::Hash, cmp::PartialEq)]
+#[derive(Clone, Debug, cmp::Eq, hash::Hash, cmp::PartialEq)]
 struct UseData {
     vis: syn::Visibility,
     attrs: Vec<syn::Attribute>,
@@ -66,18 +145,22 @@ impl UseData {
 
 // *** UseValue ***
 
-#[derive(Clone, Default)]
+#[derive(Clone, Default, Debug)]
 struct UseValue {
     nodes: HashSet<UseData>,
-    paths: UseMap,
+    paths: UseBuilder,
 }
 
 // *** Error ***
 
+/// The error type returned if issues occur during [UseBuilder] operations
 #[derive(fmt::Debug)]
 pub enum Error {
+    /// A glob was found as the first entry in a use path - this is illegal Rust
     TopLevelGlob,
+    /// A group was found as the first entry in a use path - this is not supported
     TopLevelGroup,
+    /// The same use was found but with differing dual colon prefix, attributes, or visibility
     UseWithDiffAttr,
 }
 
@@ -105,11 +188,12 @@ struct ItemUseBuilder {
 }
 
 impl ItemUseBuilder {
+    #[inline]
     fn add_path(&mut self, path: syn::Ident) {
         self.paths.push(path);
     }
 
-    fn into_item_use(mut self, mut names: Vec<UseKey>, data: UseData) -> syn::ItemUse {
+    fn into_item_use(mut self, names: Vec<UseKey>, data: UseData) -> syn::ItemUse {
         let key_to_tree = |key| match key {
             UseKey::Name(name) => syn::UseTree::Name(syn::UseName { ident: name }),
             UseKey::Rename(name, rename) => syn::UseTree::Rename(syn::UseRename {
@@ -129,7 +213,8 @@ impl ItemUseBuilder {
             })
         // If a single entry then it is either a name or rename
         } else if names.len() == 1 {
-            key_to_tree(names.remove(0))
+            // Panic safety: we verified there is exactly one item in the set
+            key_to_tree(names.into_iter().next().unwrap())
         // Group
         } else {
             let items = names.into_iter().map(key_to_tree).collect();
@@ -173,18 +258,22 @@ impl ItemUseBuilder {
 
 // *** UseMap ***
 
+/// Type that contains a partitioned list of uses by std, external, and crate level
 pub type StdExtCrateUse = (Vec<syn::ItemUse>, Vec<syn::ItemUse>, Vec<syn::ItemUse>);
 
-#[derive(Clone, Default)]
-pub struct UseMap {
-    map: HashMap<UseKey, UseValue>,
+/// A type that builds vecs of [syn::ItemUse]. It takes a [Vec] of [UseItems] as input, ensures no
+/// conflicting duplicates, groups them, and then emits as [Vec] (or multiple [Vec]) of [syn::ItemUse]
+#[derive(Clone, Default, Debug)]
+pub struct UseBuilder {
+    map: IndexMap<UseKey, UseValue>,
     entries: usize,
 }
 
-impl UseMap {
+impl UseBuilder {
+    /// Create a new builder from a [Vec] of [UseItems]
     pub fn from_uses(items: Vec<UseItems>) -> Self {
         let mut root_map = Self {
-            map: HashMap::new(),
+            map: IndexMap::new(),
             entries: 0,
         };
 
@@ -200,11 +289,11 @@ impl UseMap {
 
     fn add_node(&mut self, entry: UseKey, data: UseData) {
         match self.map.entry(entry) {
-            Entry::Occupied(mut e) => {
-                self.entries += 1;
+            indexmap::map::Entry::Occupied(mut e) => {
                 e.get_mut().nodes.insert(data);
             }
-            Entry::Vacant(e) => {
+            indexmap::map::Entry::Vacant(e) => {
+                self.entries += 1;
                 let mut u = UseValue::default();
                 u.nodes.insert(data);
                 e.insert(u);
@@ -212,10 +301,10 @@ impl UseMap {
         }
     }
 
-    fn add_path(&mut self, entry: UseKey) -> &mut UseMap {
+    fn add_path(&mut self, entry: UseKey) -> &mut UseBuilder {
         match self.map.entry(entry) {
-            Entry::Occupied(e) => &mut e.into_mut().paths,
-            Entry::Vacant(e) => {
+            indexmap::map::Entry::Occupied(e) => &mut e.into_mut().paths,
+            indexmap::map::Entry::Vacant(e) => {
                 let u = UseValue::default();
                 &mut e.insert(u).paths
             }
@@ -249,11 +338,11 @@ impl UseMap {
     }
 
     fn next_map(
-        use_map: UseMap,
-        mut builder: ItemUseBuilder,
+        use_map: UseBuilder,
+        builder: ItemUseBuilder,
         items: &mut Vec<syn::ItemUse>,
     ) -> Result<(), Error> {
-        let mut map: HashMap<UseData, Vec<UseKey>> = HashMap::new();
+        let mut map: IndexMap<UseData, Vec<UseKey>> = IndexMap::new();
         let len = use_map.map.len();
 
         // Node Strategy: try to combine as we loop over
@@ -262,8 +351,10 @@ impl UseMap {
 
             // Ignore anything but names for future paths (others are invalid as paths)
             if let UseKey::Name(path) = key.clone() {
+                // Create a builder from the original
+                let mut builder = builder.clone();
                 builder.add_path(path);
-                if let err @ Err(_) = Self::next_map(value.paths, builder.clone(), items) {
+                if let err @ Err(_) = Self::next_map(value.paths, builder, items) {
                     return err;
                 }
             }
@@ -280,10 +371,10 @@ impl UseMap {
                 // Insert into our map
                 // Panic safety: we confirmed above there is exactly one entry
                 match map.entry(value.nodes.into_iter().next().unwrap()) {
-                    Entry::Occupied(mut e) => {
+                    indexmap::map::Entry::Occupied(mut e) => {
                         e.get_mut().push(key);
                     }
-                    Entry::Vacant(e) => {
+                    indexmap::map::Entry::Vacant(e) => {
                         let mut set = Vec::with_capacity(len);
                         set.push(key);
                         e.insert(set);
@@ -301,15 +392,18 @@ impl UseMap {
         Ok(())
     }
 
-    pub fn optimize(self) -> Result<Vec<syn::ItemUse>, Error> {
+    /// Consume this builder an emit a [Vec] of [syn::ItemUse]
+    pub fn into_items(self) -> Result<Vec<syn::ItemUse>, Error> {
         let mut items = Vec::with_capacity(self.entries);
         let builder = ItemUseBuilder::default();
         Self::next_map(self, builder, &mut items)?;
         Ok(items)
     }
 
-    pub fn optimize_sections(self) -> Result<StdExtCrateUse, Error> {
-        let items = self.optimize()?;
+    /// Consume this builder and emit three vectors of [syn::ItemUse] partitioned by crate type:
+    /// std, external, and intra-crate uses
+    pub fn into_items_sections(self) -> Result<StdExtCrateUse, Error> {
+        let items = self.into_items()?;
 
         // Will be too big - better too big than too small
         let mut std_uses = Vec::with_capacity(items.len());
@@ -344,4 +438,74 @@ impl UseMap {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use assert_unordered::assert_eq_unordered;
+    use quote::quote;
+
+    use crate::{UseBuilder, UseItems};
+
+    fn make_builder() -> UseBuilder {
+        let use1 = quote! {
+            use crate::Test;
+            use std::error::Error as StdError;
+            use std::fmt::Debug;
+        };
+
+        let use2 = quote! {
+            use syn::ItemUse;
+            use std::fmt::Display;
+            use crate::*;
+        };
+
+        let items1: UseItems = syn::parse2(use1).unwrap();
+        let items2: UseItems = syn::parse2(use2).unwrap();
+
+        UseBuilder::from_uses(vec![items1, items2])
+    }
+
+    #[test]
+    fn items() {
+        let builder = make_builder();
+        //eprintln!("{:#?}", &builder);
+        let uses = builder.into_items().unwrap();
+        //println!("{uses:#?}");
+
+        let expected = quote! {
+            use crate::*;
+            use std::error::Error as StdError;
+            use std::fmt::{Debug, Display};
+            use syn::ItemUse;
+        };
+        let expected = syn::parse2::<UseItems>(expected).unwrap().into_inner();
+
+        assert_eq_unordered!(expected, uses);
+    }
+
+    #[test]
+    fn items_separated() {
+        let builder = make_builder();
+        let (std_use, ext_use, crate_use) = builder.into_items_sections().unwrap();
+
+        let std_expected = quote! {
+            use std::error::Error as StdError;
+            use std::fmt::{Debug, Display};
+        };
+        let std_expected = syn::parse2::<UseItems>(std_expected).unwrap().into_inner();
+
+        let ext_expected = quote! {
+            use syn::ItemUse;
+        };
+        let ext_expected = syn::parse2::<UseItems>(ext_expected).unwrap().into_inner();
+
+        let crate_expected = quote! {
+            use crate::*;
+        };
+        let crate_expected = syn::parse2::<UseItems>(crate_expected)
+            .unwrap()
+            .into_inner();
+
+        assert_eq_unordered!(std_expected, std_use);
+        assert_eq_unordered!(ext_expected, ext_use);
+        assert_eq_unordered!(crate_expected, crate_use);
+    }
+}
